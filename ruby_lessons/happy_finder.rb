d72b2f6782a3c97f58cf57361065e58fcbe134a5
matvey <matvejbykov267@gmail.com>
2025-10-08 19:39:09 +0700

    + one more lesson

---
+++ ruby_lessons/happy_finder.rb
@@ -0,0 +1,30 @@
+def happy?(number)
+  sum1 = 0
+  sum2 = 0
+  number_string = number.to_s
+  first_half = number_string.slice(0, number_string.length / 2)
+  second_half = number_string.slice(first_half.length..) 
+
+
+  first_half.each_char do |c|
+    sum1 += c.to_i
+  end
+
+  second_half.each_char do |c|
+    sum2 += c.to_i
+  end
+
+  sum1 == sum2 ? true : false
+
+end
+puts happy?(22211222)
+ 
+#test
+test1 = happy?(1234)
+test2 = happy?(2341)
+if test1 != false 
+  puts "test1 error #{test1}"
+end
+if test2 != true
+ puts "test2 error #{test2}"
+end
