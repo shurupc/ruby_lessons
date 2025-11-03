495228b3aa17fef0c443c3ec4aa0e7549e62d511
matvey <matvejbykov267@gmail.com>
2025-11-03 17:56:20 +0700

    new

--- ruby_lessons/method_hashless.rb
+++ ruby_lessons/method_hashless.rb
@@ -1,23 +1,36 @@
 def plainify(data)
-  data = {
-    'Queen' => [
-      'Bohemian Rhapsody',
-      "Don't Stop Me Now"
-    ],
-    'Metallica' => [
-      'Nothing Else Matters'
-    ],
-    "Guns N' Roses" => [
-      'Paradise City',
-      'November Rain'
-    ],
-    'AC/DC' => [
-      'Thunderstruck',
-      'Back In Black',
-      'Shoot to Thrill'
-    ]
-  }
-  puts data
-  data.slice(' ')
+  data.each do |key, value|
+    puts data(:band)
+  end
 end
+data = {
+  'Queen' => [
+    'Bohemian Rhapsody',
+    "Don't Stop Me Now"
+  ],
+  'Metallica' => [
+    'Nothing Else Matters'
+  ],
+  "Guns N' Roses" => [
+    'Paradise City',
+    'November Rain'
+  ],
+  'AC/DC' => [
+    'Thunderstruck',
+    'Back In Black',
+    'Shoot to Thrill'
+  ]
+}
+puts plainify data
 # test
+#test1 = plainify(data)
+#puts "#test error #{test}" if test !=  [
+#   { band: 'Queen', song: 'Bohemian Rhapsody' },
+#    { band: 'Queen', song: "Don't Stop Me Now" },
+#    { band: 'Metallica', song: 'Nothing Else Matters' },
+#    { band: "Guns N' Roses", song: 'Paradise City' },
+#   { band: "Guns N' Roses", song: 'November Rain' },
+#    { band: 'AC/DC', song: 'Thunderstruck' },
+#    { band: 'AC/DC', song: 'Back In Black' },
+#    { band: 'AC/DC', song: 'Shoot to Thrill' }
+#  ]
